4bad109460f5aa75b5eed7e8983131f53dda69a2
Hector Chu <hectorchu@gmail.com>
2019-06-26 20:56:40 +0100

    Fix

--- shell/cmds/index.js
+++ shell/cmds/index.js
@@ -2,6 +2,9 @@ const cat = require('./cat')
 const chdir = require('./chdir')
 const exec = require('./exec')
 const ls = require('./ls')
+const mkdir = require('./mkdir')
+const rm = require('./rm')
+const rmdir = require('./rmdir')
 
 module.exports = {
   cat,
@@ -9,4 +12,7 @@ module.exports = {
   chdir,
   exec,
   ls,
+  mkdir,
+  rm,
+  rmdir,
 }
