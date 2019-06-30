793bd1cbaed50fa1f63885bfb337e79350950847
Hector Chu <hectorchu@gmail.com>
2019-06-30 16:09:57 +0100

    Streamline some functions

--- shell/cmds/cat.js
+++ shell/cmds/cat.js
@@ -6,15 +6,11 @@ module.exports = async (web3, kernel, cmd, args) => {
     const data = {}
     const path = enc(x)
     const {entries} = await kernel.stat(path)
-    const keys = []
     for (let i = 0; i < entries; i++) {
-      keys.push(await kernel.readkeyPath(path, i))
-    }
-    await keys.reduce(async (promise, key) => {
-      await promise
+      const key = await kernel.readkeyPath(path, i)
       const value = await kernel.readPath(path, key)
       data[dec(key)] = dec(value)
-    }, Promise.resolve())
+    }
     console.log(JSON.stringify(data, null, 2))
   }, Promise.resolve())
 }
--- shell/utils/enc.js
+++ shell/utils/enc.js
@@ -2,5 +2,5 @@ const {asciiToHex, hexToAscii} = require('web3-utils')
 
 module.exports = {
   enc: x => asciiToHex(x),
-  dec: x => hexToAscii(x).replace(/\0+$/, ''),
+  dec: x => hexToAscii(x),
 }
