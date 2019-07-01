f3ae04d7c39bda88ac13245dcec03b911d9c1d0b
Hector Chu <hectorchu@gmail.com>
2019-07-01 21:08:07 +0100

    Rename TestDapp to hello_world

--- contracts/bin/HelloWorld.sol
+++ contracts/bin/HelloWorld.sol
@@ -1,19 +1,19 @@
 pragma solidity >= 0.5.8;
 
-import './interface/App.sol';
+import '../interface/App.sol';
 
-contract TestDapp is App {
+contract HelloWorld is App {
   constructor(Kernel kernel) public {
     install(kernel);
   }
 
   function install(Kernel kernel) public {
-    kernel.install(address(this), '/bin/TestDapp');
+    kernel.install(address(this), '/bin/hello_world');
   }
 
   function main(Kernel kernel, uint[] calldata, bytes calldata) external returns (uint) {
-    uint fd = kernel.open('test_file', 0x0101);
-    kernel.write(fd, 'foo', 'bar');
+    uint fd = kernel.open('hello_world.txt', 0x0101);
+    kernel.write(fd, 'Hello', 'World!');
     kernel.close(fd);
   }
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,17 +1,16 @@
 const FileSystem = artifacts.require("FileSystemImpl");
 const Kernel = artifacts.require("KernelImpl");
-
 const SetupDirs = artifacts.require("SetupDirs");
-const TestDapp = artifacts.require("TestDapp");
 
 const Copy = artifacts.require("Copy");
 const Move = artifacts.require("Move");
+const HelloWorld = artifacts.require("HelloWorld");
 
 module.exports = function(deployer) {
   deployer.deploy(FileSystem)
     .then(() => deployer.deploy(Kernel, FileSystem.address))
     .then(() => deployer.deploy(SetupDirs, Kernel.address))
-    .then(() => deployer.deploy(TestDapp, Kernel.address))
     .then(() => deployer.deploy(Copy, Kernel.address))
     .then(() => deployer.deploy(Move, Kernel.address))
+    .then(() => deployer.deploy(HelloWorld, Kernel.address))
 };
