87fb844d1610664b907cc9f7d61644f4fc313194
Hector Chu <hectorchu@gmail.com>
2019-07-02 17:41:02 +0100

    Tab complete commands

--- shell/utils/completer.js
+++ shell/utils/completer.js
@@ -1,12 +1,26 @@
+const cmds = require('../cmds')
 const listPath = require('./listPath')
 
 module.exports = async (kernel, line, callback) => {
-  const path = (line.match(/\S+$/) || [''])[0]
+  let path
+  const emptyOrOneWord = line.match(/^\s*(\S+)?$/)
+  if (emptyOrOneWord) {
+    path = emptyOrOneWord[1] || ''
+  } else {
+    const lastWord = line.match(/\S+$/)
+    path = lastWord ? lastWord[0] : ''
+  }
   const index = path.lastIndexOf('/') + 1
   const dirname = path.slice(0, index) || '.'
   const basename = path.slice(index)
   try {
-    const keys = await listPath(kernel, dirname)
+    let keys = new Set(await listPath(kernel, dirname))
+    if (emptyOrOneWord) {
+      const bin = await listPath(kernel, '/bin')
+      bin.forEach(x => keys.add(x))
+      for (x in cmds) keys.add(x)
+    }
+    keys = Array.from(keys).sort()
     const hits = keys.filter(x => x.startsWith(basename))
     callback(null, [hits.length ? hits : keys, basename])
   } catch (e) {
