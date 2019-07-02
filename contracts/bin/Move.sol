a0c3d8297156b72b0df944f31e6c4cac259af31b
Hector Chu <hectorchu@gmail.com>
2019-07-02 21:04:54 +0100

    Fix calling conventions

--- contracts/bin/Move.sol
+++ contracts/bin/Move.sol
@@ -13,18 +13,20 @@ contract Move is App {
 
   function main(Kernel kernel, uint[] calldata argi, bytes calldata args) external returns (uint) {
     require(argi.length >= 2, 'EINVAL');
-    bytes memory target = new bytes(args.length - argi[argi.length-1]);
-    for (uint i = 0; i < target.length; i++) {
-      target[i] = args[argi[argi.length-1] + i];
+    uint index = argi[argi.length-2];
+    bytes memory target = new bytes(argi[argi.length-1] - index);
+    for (uint i; i < target.length; i++) {
+      target[i] = args[index + i];
     }
     if (argi.length > 2) {
       (FileSystem.FileType fileType,,,,,,,) = kernel.stat(target);
       require(fileType == FileSystem.FileType.Directory, 'ENOTDIR');
     }
-    for (uint i = 0; i < argi.length-1; i++) {
-      bytes memory source = new bytes(argi[i+1] - argi[i]);
-      for (uint j = 0; j < source.length; j++) {
-        source[j] = args[argi[i] + j];
+    for (uint i; i < argi.length-1; i++) {
+      index = i > 0 ? argi[i-1] : 0;
+      bytes memory source = new bytes(argi[i] - index);
+      for (uint j; j < source.length; j++) {
+        source[j] = args[index + j];
       }
       kernel.move(source, target);
     }
